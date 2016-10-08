eaf8c479a89d498966e2ba0ec1bab944e64ecd14
Gabriel SULLICE <gabriel@sullice.com>
2016-10-08 10:35:41 -0600

    Brings rethinkdb driver in line with new storage work

--- storage/rethinkdb/rethinkdb.go
+++ storage/rethinkdb/rethinkdb.go
@@ -3,17 +3,57 @@ package rethinkdb
 import (
 	"context"
 
+	"github.com/dancannon/gorethink"
 	"github.com/gabesullice/phargo/storage"
 )
 
-type Client struct{}
+// A Client can connect to and execute queries against a RethinkDB instance.
+type Client struct {
+	session *session
+}
+
+type preparedQuery struct {
+	query   term
+	session *session
+}
+
+type session gorethink.Session
+
+type term gorethink.Term
 
 type result struct{}
 
-func (c Client) Do(ctx context.Context, q storage.QueryCompiler) (storage.Fetcher, error) {
+// Do takes a runnable query and executes it. A Client may have timeouts and
+// other custom configuration defined, e.g. retry logic.
+func (c Client) Do(ctx context.Context, q storage.Runner) (storage.Fetcher, error) {
+	return q.Run()
+}
+
+// Prepare takes a standard query and turns it into a query that can be run
+// against Client's connection to RethinkDB.
+func (c Client) Prepare(q storage.Query) (storage.Runner, error) {
+	return c.compile(q)
+}
+
+// Execute attempt to prepare a standard Query and then immediately executes it,
+// returning a Fetchable response.
+func (c Client) Execute(q storage.Query) (storage.Fetcher, error) {
+	p, err := c.Prepare(q)
+	if err != nil {
+		return nil, err
+	}
+	return p.Run()
+}
+
+func (c Client) compile(q storage.Query) (storage.Runner, error) {
+	return preparedQuery{term{}, c.session}, nil
+}
+
+func (q preparedQuery) Run() (storage.Fetcher, error) {
 	return result{}, nil
 }
 
-func (r result) Fetch(target interface{}) error      { return nil }
-func (r result) FetchOne(target interface{}) error   { return nil }
-func (r result) FetchAll(target []interface{}) error { return nil }
+func (r result) Fetch(target interface{}) (more bool, err error) { return }
+func (r result) FetchOne(target interface{}) (err error)         { return }
+func (r result) FetchAll(target []interface{}) (err error)       { return }
+func (r result) Empty() bool                                     { return false }
--- storage/rethinkdb/rethinkdb_test.go
+++ storage/rethinkdb/rethinkdb_test.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"testing"
 
+	"github.com/pkg/errors"
+
 	"github.com/gabesullice/phargo/storage"
 )
 
@@ -11,12 +13,20 @@ func TestDo(t *testing.T) {
 	t.Run("Do should compile and run a query.", func(t *testing.T) {
 		var c storage.Client = Client{}
 		var compiled, run bool
-		q := MockQuery{compilerFunc(&compiled), runnerFunc(&run, false)}
+		q := MockQuery{func() {
+			compiled = true
+		}, func() bool {
+			run = true
+			return false
+		}}
 		_, err := c.Do(context.Background(), q)
 		if err != nil {
 			t.Errorf("Expected success, got error: %v", err)
 		}
-		if !q.ran() {
+		if !compiled {
+			t.Error("Expected query to be compiled.")
+		}
+		if !run {
 			t.Error("Expected query to be run.")
 		}
 	})
@@ -37,17 +47,4 @@ func (q MockQuery) Run() (res storage.Fetcher, err error) {
 		err = errors.New("MockQuery.Run() failed.")
 	}
 	return result{}, err
-}
-
-func compilerFunc(compiled *bool) {
-	return func() {
-		compiled = true
-	}
-}
-
-func runnerFunc(ran *bool, shouldFail bool) {
-	return func() bool {
-		ran = true
-		return shouldFail
-	}
 }
